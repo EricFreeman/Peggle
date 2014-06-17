8fb4deb9fff1c2635dc5b66cde578071741efe86
Eric Freeman <eric.freeman@extendhealth.com>
2014-06-17 08:28:09 -0600

    Added pegboard and ball drops and rolls off of them

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Board : MonoBehaviour
+    {
+        public int BoardSize = 10;
+
+        void Start()
+        {
+            for (var x = 0; x < BoardSize; x++)
+            {
+                for (var y = 0; y < BoardSize; y++)
+                {
+                    var obj = (GameObject)Instantiate(Resources.Load("prefabs/peg"));
+                    obj.transform.position = new Vector3(x, y, 0);
+                }
+            }
+        }
+    }
+}

d0288215f23eb135c3a464ab203caf729a5324a9
Eric Freeman <eric.freeman@extendhealth.com>
2014-06-17 18:13:44 -0600

    Now you can click to drop balls.  no time to make they shoot out because train is almost at my stop.

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Gun : MonoBehaviour
+    {
+        public Transform Barrel;
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var ball = (GameObject)Instantiate(Resources.Load("prefabs/ball"));
+                ball.transform.position = Barrel.position;
+            }
+        }
+    }
+}

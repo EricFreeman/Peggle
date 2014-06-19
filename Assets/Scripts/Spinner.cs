93a542221ddc3f0f7ffd4e8e284c106978fcb13d
Eric Freeman <eric.freeman@extendhealth.com>
2014-06-19 08:39:11 -0600

    Added a second level with a spinning thing in the center.

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Spinner : MonoBehaviour
+    {
+        public float SpinSpeed = 50;
+
+        void FixedUpdate()
+        {
+            transform.Rotate(0, 0, SpinSpeed * Time.deltaTime);
+        }
+    }
+}
